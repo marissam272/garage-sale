0b03d0780d38f79f600f8d8bc4eb47b81d62cfaa
Riya Abraham <riyamaryabraham12@gmail.com>
2018-07-11 14:52:54 -0500

    modified js

--- public/js/seller_manager.js
+++ public/js/seller_manager.js
@@ -1,146 +1,87 @@
 
 $(document).ready(function() {
-    // Getting a reference to the input field where user adds a new todo
-    var $newItemInput = $("input.new-item");
-    // Our new todos will go inside the todoContainer
-    var $productContainer = $(".product-container");
-    // Adding event listeners for deleting, editing, and adding todos
-    $(document).on("click", "button.delete", deleteProduct);
-    $(document).on("click", "button.complete", toggleComplete);
-    $(document).on("click", ".product-item", editProduct);
-    $(document).on("keyup", ".product-item", finishEdit);
-    $(document).on("blur", ".product-item", cancelEdit);
-    $(document).on("submit", "#product-form", insertProduct);
-  
-    // Our initial todos array
-    var products = [];
-  
-    // Getting todos from database when page loads
-    getProducts();
-  
-    // This function resets the todos displayed with new todos from the database
-    function initializeRows() {
-      $productContainer.empty();
-      var rowsToAdd = [];
-      for (var i = 0; i < products.length; i++) {
-        rowsToAdd.push(createNewRow(products[i]));
-      }
-      $productContainer.prepend(rowsToAdd);
+  // Gets an optional query string from our url (i.e. ?product_id=23)
+  var url = window.location.search;
+  var productId;
+  // Sets a flag for whether or not we're updating a post to be false initially
+  var updating = false;
+
+  // If we have this section in our url, we pull out the product id from the url
+  // In localhost:8080/seller_manager?product_id=1, productId is 1
+  if (url.indexOf("?product_id=") !== -1) {
+    productId = url.split("=")[1];
+    getProductData(productId);
+  }
+
+  // Getting jQuery references to the product name,description,category,price
+  var nameInput = $("#name");
+  var descriptionInput = $("#description");
+  var productForm = $("#product-form");
+  var priceValue = $("#price");
+  var imagevalue=$("#img");
+            // Giving the postCategorySelect a default value
+            // postCategorySelect.val("Personal");
+  // Adding an event listener for when the form is submitted
+  $(productForm).on("submit", function handleFormSubmit(event) {
+    event.preventDefault();
+    // Wont submit the post if we are missing a body or a title
+    if (!nameInput.val().trim() || !descriptionInput.val().trim() || !priceValue.val().trim()
+          || !imagevalue.val().trim()) {
+      return;
     }
-  
-    // This function grabs todos from the database and updates the view
-    function getProducts() {
-      $.get("/api/products", function(data) {
-        products = data;
-        initializeRows();
-      });
+    // Constructing a newProductt object to hand to the database
+    var newProduct = {
+      name: nameInput.val().trim(),
+      description: descriptionInput.val().trim(),
+      price: priceValue.val(),
+      img:imagevalue.val()
+    };
+
+    console.log(newProduct);
+
+    // If we're updating a post run updatePost to update a post
+    // Otherwise run submitPost to create a whole new post
+    if (updating) {
+      newProduct.id = productId;
+      updateProduct(newProduct);
     }
-  
-    // This function deletes a todo when the user clicks the delete button
-    function deleteProduct(event) {
-      event.stopPropagation();
-      var id = $(this).data("id");
-      $.ajax({
-        method: "DELETE",
-        url: "/api/products/" + id
-      }).then(getProducts);
-    }
-  
-    // This function handles showing the input box for a user to edit a todo
-    function editProduct() {
-      var currentProduct = $(this).data("product");
-      $(this).children().hide();
-      $(this).children("input.edit").val(currentProduct.name);
-      $(this).children("input.edit").val(currentProduct.description);
-      $(this).children("input.edit").val(currentProduct.price);
-      $(this).children("input.edit").val(currentProduct.img);
-      $(this).children("input.edit").show();
-      $(this).children("input.edit").focus();
-    }
-  
-    // Toggles complete status
-    function toggleComplete(event) {
-      event.stopPropagation();
-      var product = $(this).parent().data("product");
-      product.complete = !product.complete;
-      updateProduct(product);
-    }
-  
-    // This function starts updating a todo in the database if a user hits the "Enter Key"
-    // While in edit mode
-    function finishEdit(event) {
-      var updatedProduct = $(this).data("product");
-      if (event.which === 13) {
-        updatedProduct.name = $(this).children("input").val().trim();
-        updatedProduct.description = $(this).children("input").val().trim();
-        updatedProduct.price = $(this).children("input").val().trim();
-        updatedProduct.img = $(this).children("input").val().trim();
-        $(this).blur();
-        updateProduct(updatedProduct);
-      }
-    }
-  
-    // This function updates a todo in our database
-    function updateProduct(product) {
-      $.ajax({
-        method: "PUT",
-        url: "/api/products",
-        data: product
-      }).then(getProducts);
-    }
-  
-    // This function is called whenever a todo item is in edit mode and loses focus
-    // This cancels any edits being made
-    function cancelEdit() {
-      var currentProduct = $(this).data("product");
-      if (currentProduct) {
-        $(this).children().hide();
-        $(this).children("input.edit").val(currentProduct.name);
-        $(this).children("input.edit").val(currentProduct.description);
-        $(this).children("input.edit").val(currentProduct.price);
-        $(this).children("input.edit").val(currentProduct.img);
-        $(this).children("span").show();
-        $(this).children("button").show();
-      }
-    }
-  
-    // This function constructs a todo-item row
-    function createNewRow(product) {
-      var $newInputRow = $(
-        [
-          "<li class='list-group-item product-item'>",
-          "<span>",
-          product.name,
-          product.description,
-          product.price,
-          product.img,
-          "</span>",
-          "<input type='text' class='edit' style='display: none;'>",
-          "<button class='delete btn btn-danger'>x</button>",
-          "<button class='complete btn btn-primary'>✓</button>",
-          "</li>"
-        ].join("")
-      );
-      
-  
-      $newInputRow.find("button.delete").data("id", product.id);
-      $newInputRow.find("input.edit").css("display", "none");
-      $newInputRow.data("product", product);
-      if (product.complete) {
-        $newInputRow.find("span").css("text-decoration", "line-through");
-      }
-      return $newInputRow;
-    }
-  
-    // This function inserts a new todo into our database and then updates the view
-    function insertProduct(event) {
-      event.preventDefault();
-      var product = {
-        text: $newItemInput.val().trim(),
-        complete: false
-      };
-  
-      $.post("/api/products", product, getProducts);
-      $newItemInput.val("");
+    else {
+      submitProduct(newProduct);
     }
   });
+
+  // Submits a new post and brings user to blog page upon completion
+  function submitProduct(product) {
+    $.post("/api/products/", product, function() {
+      window.location.href = "/view_products";
+    });
+  }
+
+  // Gets product data for a product if we're editing
+  function getProductData(id) {
+    $.get("/api/products/" + id, function(data) {
+      if (data) {
+        // If this product exists, prefill our viewproduct forms with its data
+        nameInput.val(data.name);
+        descriptionInput.val(data.description);
+        priceValue.val(data.price);
+        imagevalue.val(data.img);
+        // If we have a product with this id, set a flag for us to know to update the product
+        // when we hit submit
+        updating = true;
+      }
+    });
+  }
+
+  // Update a given product, bring user to the view page when done
+  function updateProduct(product) {
+    $.ajax({
+      method: "PUT",
+      url: "/api/products",
+      data: product
+    })
+      .then(function() {
+        window.location.href = "/view_products";
+      });
+  }
+});
