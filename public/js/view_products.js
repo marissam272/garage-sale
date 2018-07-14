d603e4c8b3e9b5c8886807a802831c37d73ff268
Riya Abraham <riyamaryabraham12@gmail.com>
2018-07-13 19:29:51 -0500

    product -description got to work

--- public/js/view_products.js
+++ public/js/view_products.js
@@ -3,94 +3,91 @@ $(document).ready(function() {
   // productContainer holds all of our posts
   var productContainer = $(".product-container");
   //var postCategorySelect = $("#category");
- 
-  $(document).on("click","button.buy",handleProductPurchase);
-  
-  $(document).on("click", ".product_container", handleView);
-  
-  
+  $(document).on("click","button.buy",handleProductPurchase); 
   var products;
 
   /////////////////////////////////////////////////
+  //======================================================
   // product_description.html js code
-  var description = $(".description_container");
-  var product;
-  function getProduct() {
+//   $(document).on("click", ".product_container", handleView);
+//   var description = $(".description_container");
+//   var product;
+//   function getProduct() {
       
-    $.get("/api/products/:id", function(data) {
-      console.log("Products", data);
-      product = data;
-      if (!product || !product.length) {
-        displayEmpty();
-      }
-      else {
-        initializeRow();
-      }
-    });
-  }
-//fuunction to handle buy functionality
-function handleView() {
-  var currentProduct = $(this)
-    .parent()
-    .parent()
-    .data("product");
-    console.log(currentProduct.id);
+//     $.get("/api/products/:id", function(data) {
+//       console.log("Products", data);
+//       product = data;
+//       if (!product || !product.length) {
+//         displayEmpty();
+//       }
+//       else {
+//         initializeRow();
+//       }
+//     });
+//   }
+// //fuunction to handle buy functionality
+// function handleView() {
+//   var currentProduct = $(this)
+//     .parent()
+//     .parent()
+//     .data("product");
+//     console.log(currentProduct.id);
     
-    $.ajax({
-      method: "PUT",
-      url: "/api/products/:id" + currentProduct.id,
-      checkout: true
-    }).then(window.location.href = "/product_description.html?product_id=" + currentProduct.id);
-    //window.location.href = "/checkout?product_id=" + currentProduct.id;
-}
+//     $.ajax({
+//       method: "PUT",
+//       url: "/api/products/:id" + currentProduct.id,
+//       checkout: true
+//     }).then(window.location.href = "/product_description.html?product_id=" + currentProduct.id);
+//     //window.location.href = "/checkout?product_id=" + currentProduct.id;
+// }
 
-  getProduct();
+//   getProduct();
 
-  function initializeRow() {
-    description.empty();
-    var productToAdd = [];
-    productToAdd.push(createNewRow1(product));
-    description.append(productToAdd);
-  }
+//   function initializeRow() {
+//     description.empty();
+//     var productToAdd = [];
+//     productToAdd.push(createNewRow1(product));
+//     description.append(productToAdd);
+//   }
 
-  // This function constructs a post's HTML
-  function createNewRow1(product) {
-    var newProductCard = $("<div class='container'>");
-    newProductCard.addClass("card");
-    var newProductCardHeading = $("<div>");
-    newProductCardHeading.addClass("card-header");
+//   // This function constructs a post's HTML
+//   function createNewRow1(product) {
+//     var newProductCard = $("<div class='container'>");
+//     newProductCard.addClass("card");
+//     var newProductCardHeading = $("<div>");
+//     newProductCardHeading.addClass("card-header");
 
-    var buyBtn = $("<button>");
-    buyBtn.text("INTERESTED ? BUY IT");
-    buyBtn.addClass("buy btn btn-success");
-    var newProductName = $("<h2>");
+//     var buyBtn = $("<button>");
+//     buyBtn.text("INTERESTED ? BUY IT");
+//     buyBtn.addClass("buy btn btn-success");
+//     var newProductName = $("<h2>");
     
-    var newProductCardBody = $("<div>");
-    newProductCardBody.addClass("card-body");
+//     var newProductCardBody = $("<div>");
+//     newProductCardBody.addClass("card-body");
 
-    var newProductBody = $("<p>");
-    var newProductPrice = $("<p>");
-    var newProductImg = $("<p>");
+//     var newProductBody = $("<p>");
+//     var newProductPrice = $("<p>");
+//     var newProductImg = $("<p>");
 
-    newProductName.text(product.name + " ");
-    newProductBody.text(product.description);
-    newProductPrice.text(product.price);
-    newProductImg.text(product.img);
+//     newProductName.text(product.name + " ");
+//     newProductBody.text(product.description);
+//     newProductPrice.text(product.price);
+//     newProductImg.text(product.img);
 
-    newProductCardHeading.append(newProductName);
-    newProductCardHeading.append(buyBtn);
+//     newProductCardHeading.append(newProductName);
+//     newProductCardHeading.append(buyBtn);
 
-    newProductCardBody.append(newProductBody);
-    newProductCardBody.append(newProductPrice);
-    newProductCardBody.append(newProductImg);
-    newProductCard.append(newProductCardHeading);
-    newProductCard.append(newProductCardBody);
-    newProductCard.data("product", product);
-    return newProductCard;
-  }
+//     newProductCardBody.append(newProductBody);
+//     newProductCardBody.append(newProductPrice);
+//     newProductCardBody.append(newProductImg);
+//     newProductCard.append(newProductCardHeading);
+//     newProductCard.append(newProductCardBody);
+//     newProductCard.data("product", product);
+//     return newProductCard;
+//   }
 // end product_description.html js code
 /////////////////////////////////////////////////////
-
+//=============================================
 
   // This function grabs posts from the database and updates the view
   function getProducts() {
@@ -107,17 +104,7 @@ function handleView() {
     });
   }
 
-  // This function does an API call to delete posts
-  function deleteProduct(id) {
-    $.ajax({
-      method: "DELETE",
-      url: "/api/products/" + id
-    })
-      .then(function() {
-        getProducts();
-      });
-  }
-
+  
   // Getting the initial list of products
   getProducts();
   // InitializeRows handles appending all of our constructed post HTML inside
@@ -172,26 +159,7 @@ function handleView() {
     return newProductCard;
   }
 
-  // This function figures out which post we want to delete and then calls
-  // deletePost
-  function handleProductDelete() {
-    var currentProduct = $(this)
-      .parent()
-      .parent()
-      .data("product");
-    deleteProduct(currentProduct.id);
-  }
-
-  // This function figures out which post we want to edit and takes it to the
-  // Appropriate url
-  function handleProductEdit() {
-    var currentProduct = $(this)
-      .parent()
-      .parent()
-      .data("product");
-    window.location.href = "/seller_manager?product_id=" + currentProduct.id;
-  }
-
+  
   //fuunction to handle buy functionality
   function handleProductPurchase() {
     var currentProduct = $(this)
